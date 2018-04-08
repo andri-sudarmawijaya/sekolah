075cf11e6c1bd32cd0ed5d4e7ef967b8949cc8fb
arifin09 <kadenzo.ilham@gmail.com>
2018-04-08 23:15:43 +0700

    update seeds
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	new file:   src/database/seeds/BantenprovJenisSekolahSeeder.csv
    	modified:   src/database/seeds/BantenprovJenisSekolahSeeder.php
    	deleted:    src/database/seeds/BantenprovJenisSekolahSeederJenisSekolah.php
    	new file:   src/database/seeds/BantenprovProdiSekolahSeeder.csv
    	modified:   src/database/seeds/BantenprovProdiSekolahSeeder.php
    	deleted:    src/database/seeds/BantenprovProdiSekolahSeederProdiSekolah.php

--- src/database/seeds/BantenprovJenisSekolahSeeder.php
+++ src/database/seeds/BantenprovJenisSekolahSeeder.php
@@ -1,9 +1,32 @@
 <?php
-
 use Illuminate\Database\Seeder;
-
+/**
+ * Usage :
+ * [1] $ composer dump-autoload -o
+ * [2] $ php artisan db:seed --class=BantenprovJenisSekolahSeeder
+ */
 class BantenprovJenisSekolahSeeder extends Seeder
 {
+    /* text color */
+    protected $RED     ="\033[0;31m";
+    protected $CYAN    ="\033[0;36m";
+    protected $YELLOW  ="\033[1;33m";
+    protected $ORANGE  ="\033[0;33m";
+    protected $PUR     ="\033[0;35m";
+    protected $GRN     ="\e[32m";
+    protected $WHI     ="\e[37m";
+    protected $NC      ="\033[0m";
+    /* File name */
+    /* location : /databse/seeds/file_name.csv */
+    protected $fileName = "BantenprovJenisSekolahSeeder.csv";
+    /* text info : default (true) */
+    protected $textInfo = true;
+    /* model class */
+    protected $model;
+    /* __construct */
+    public function __construct(){
+        $this->model = new Bantenprov\Sekolah\Models\Bantenprov\Sekolah\JenisSekolah;
+    }
     /**
      * Run the database seeds.
      *
@@ -11,6 +34,57 @@ class BantenprovJenisSekolahSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BantenprovJenisSekolahSeederJenisSekolah::class);
+        $this->insertData();
+    }
+    /* function insert data */
+    protected function insertData()
+    {
+        /* silahkan di rubah sesuai kebutuhan */
+        foreach($this->readCSV() as $data){
+
+            
+        	$this->model->create([
+            	'jenis_sekolah' => $data['jenis_sekolah'],
+				'user_id' => $data['user_id'],
+
+        	]);
+        
+
+        }
+
+        if($this->textInfo){                
+            echo "============[DATA]============\n";
+            $this->orangeText('jenis_sekolah : ').$this->greenText($data['jenis_sekolah']);
+			echo"\n";
+			$this->orangeText('user_id : ').$this->greenText($data['user_id']);
+			echo"\n";
+        
+            echo "============[DATA]============\n\n";
+        }
+
+        $this->greenText('[ SEEDER DONE ]');
+        echo"\n\n";
+    }
+    /* text color: orange */
+    protected function orangeText($text)
+    {
+        printf($this->ORANGE.$text.$this->NC);
+    }
+    /* text color: green */
+    protected function greenText($text)
+    {
+        printf($this->GRN.$text.$this->NC);
+    }
+    /* function read CSV file */
+    protected function readCSV()
+    {
+        $file = fopen(database_path("seeds/".$this->fileName), "r");
+        $all_data = array();
+        $row = 1;
+        while(($data = fgetcsv($file, 1000, ",")) !== FALSE){
+            $all_data[] = ['jenis_sekolah' => $data[0],'user_id' => $data[1],];
+        }
+        fclose($file);
+        return  $all_data;
     }
 }
