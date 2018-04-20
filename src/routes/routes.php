8b167a9e68e4d236988f8a717e26488ee62943b7
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-04-20 09:03:49 +0700

    Jenis sekolah with controller

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,5 +1,29 @@
 <?php
 
+Route::group(['prefix' => 'api/jenis-sekolah', 'middleware' => ['web']], function() {
+    $class          = 'Bantenprov\Sekolah\Http\Controllers\JenisSekolahController';
+    $name           = 'jenis-sekolah';
+    $controllers    = (object) [
+        'index'     => $class.'@index',
+        'get'       => $class.'@get',
+        'create'    => $class.'@create',
+        'show'      => $class.'@show',
+        'store'     => $class.'@store',
+        'edit'      => $class.'@edit',
+        'update'    => $class.'@update',
+        'destroy'   => $class.'@destroy',
+    ];
+
+    Route::get('/',             $controllers->index)->name($name.'.index');
+    Route::get('/get',          $controllers->get)->name($name.'.get');
+    Route::get('/create',       $controllers->create)->name($name.'.create');
+    Route::get('/{id}',         $controllers->show)->name($name.'.show');
+    Route::post('/',            $controllers->store)->name($name.'.store');
+    Route::get('/{id}/edit',    $controllers->edit)->name($name.'.edit');
+    Route::put('/{id}',         $controllers->update)->name($name.'.update');
+    Route::delete('/{id}',      $controllers->destroy)->name($name.'.destroy');
+});
+
 Route::group(['prefix' => 'api/sekolah', 'middleware' => ['web']], function() {
     $class          = 'Bantenprov\Sekolah\Http\Controllers\SekolahController';
     $name           = 'sekolah';
